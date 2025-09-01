ff67f4688ae339beb3ee348bdb633e04eaf8b856
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2025-09-01 19:23:13 -0300

    ajuste do arquivo supabase

--- supabase.js
+++ supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY; // use a anon key
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY_SERVICE
+);
