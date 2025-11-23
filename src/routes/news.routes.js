e717d642a2aa732452b1399765caf486ad1960a9
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2025-11-23 04:01:27 -0300

    proteção da rota de criação

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -4,7 +4,7 @@ import {auth} from "../middlewares/authentication.js"
 const controller = new NewsController();
 
 export async function newsRoutes(server) {
-  server.post("/news", controller.create);
+  server.post("/news", {preHandler: auth} , controller.create);
   server.get("/show-news", controller.list);
   server.put("/update-news/:id", {preHandler: auth} , controller.update);
   server.delete("/delete-news/:id", {preHandler: auth} , controller.delete);
