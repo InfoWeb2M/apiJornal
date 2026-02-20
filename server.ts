22d51cc43521937e73d28493263ddd8c23f2d6c4
Benin10-bit <beniciodoamaralneto2019@gmail.com>
2026-02-20 15:17:26 -0300

    fix'

--- server.ts
+++ server.ts
@@ -35,6 +35,6 @@ server.use(ExceptionHandler);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
